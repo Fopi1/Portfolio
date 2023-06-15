fb9be4b8101b35864f38a4037dd3148e33c648ac
Fopi1 <hfh535mail.com@gmail.com>
2023-06-15 20:49:44 +0300

    version 0.5

--- HTML/Pskovedu/script.js
+++ HTML/Pskovedu/script.js
@@ -1,5 +1,8 @@
-var pastId = "1";
-var currentId;
+let pastId = "1";
+let currentId = 0;
+let infoImgs = document.getElementsByClassName("info-img");
+let nextInfo = document.getElementById("nextInfo");
+let prevInfo = document.getElementById("prevInfo");
 
 const clickedId = (repliedId) => {
   currentId = repliedId;
@@ -67,4 +70,33 @@ const prevSlide = () => {
     .getElementById("news" + " " + String(currentId))
     .classList.add("hidden");
   pastId = Number(pastId) - 1;
+};
+
+const switchInfo = (infoNumber) => {
+  for (let i = 0; i < infoImgs.length; i++) {
+    console.log(i);
+    if (i !== infoNumber) {
+      infoImgs[i].classList.add("hidden");
+    } else {
+      infoImgs[i].classList.remove("hidden");
+    }
+  }
+};
+const validNumberOfInfo = (infoNumber) => {
+  let validNumber = infoNumber;
+  if (infoNumber < 0) {
+    validNumber = infoImgs.length - 1;
+  } else if (infoNumber > infoImgs.length - 1) {
+    validNumber = 0;
+  }
+  currentId = validNumber;
+  return currentId;
+};
+prevInfo.onclick = function () {
+  let newInfo = currentId - 1;
+  switchInfo(validNumberOfInfo(newInfo));
+};
+nextInfo.onclick = function () {
+  let newInfo = currentId + 1;
+  switchInfo(validNumberOfInfo(newInfo));
 };
